6f325970e0a08e2f9986b05ee7202b1ab38e3e7c
Abdul Mozid <csemozid@gmail.com>
2018-04-25 16:29:35 +0600

    A class mytodo is created

---
+++ new1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int size=1,currentpostion=1;
+class mytodo{
+public:
+    string description;
+    string duedate;
+    string taskname;
+};
+class mytodo mytodolist[100];
